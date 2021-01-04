4676a2157a8a2ea9f3b92a76b19784f839a1f90d
zeki47 <zeki.kcln@hotmail.com>
2021-01-04 22:31:54 +0300

    initial commit, Jan 04

---
+++ src/test/java/com/VacuumCleaner/StepDefinitions/Hooks.java
@@ -0,0 +1,38 @@
+package com.VacuumCleaner.StepDefinitions;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import com.VacuumCleaner.Utilities.Driver;
+
+
+public class Hooks {
+    @Before
+    public void setUp(){
+        System.out.println("\tthis is coming from BEFORE");
+    }
+
+    @After
+    public void tearDown(Scenario scenario){
+        if(scenario.isFailed()){
+            final byte[] screenshot = ((TakesScreenshot) Driver.get()).getScreenshotAs(OutputType.BYTES);
+            scenario.attach(screenshot,"image/png","screenshot");
+        }
+
+        Driver.closeDriver();
+
+    }
+
+    @Before("@db")
+    public void setUpdb(){
+        System.out.println("\tconnecting to database...");
+    }
+
+    @After("@db")
+    public void closeDb(){
+        System.out.println("\tdisconnecting to database...");
+
+    }
+}
---
+++ src/test/java/com/VacuumCleaner/pages/Urunler.java
@@ -0,0 +1,4 @@
+package com.VacuumCleaner.pages;
+
+public class Urunler {
+}
