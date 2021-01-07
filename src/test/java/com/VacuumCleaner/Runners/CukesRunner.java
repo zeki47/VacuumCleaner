f7749eb868524aa2a7ae9a25462b30ceba3f2d99
zeki47 <zeki.kcln@hotmail.com>
2021-01-07 13:13:32 +0300

    zeki, jan 07, 13.13

--- src/test/java/com/VacuumCleaner/Runners/CukesRunner.java
+++ src/test/java/com/VacuumCleaner/Runners/CukesRunner.java
@@ -14,7 +14,7 @@ import org.junit.runner.RunWith;
         glue = "com/VacuumCleaner/StepDefinitions",
 
         dryRun = true,
-        tags = "@urunler"
+        tags = "@navigate"
 )
 public class CukesRunner {
 }
